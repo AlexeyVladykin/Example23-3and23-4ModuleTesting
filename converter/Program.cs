4a4a76e97b159488a199004de13cc96ed5acc0c1
KirillPermyakov <permyakov.cyrill@yandex.ru>
2026-03-24 14:44:30 +0300

    свич кейс добавлен

---
+++ converter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+                (@"введите единицу измерения:");
+            switch (8) 
+            {
+                case 1:
+                    Console.WriteLine("длина");
+                case 2:
+                    Console.WriteLine("длина");
+                case 3:
+                    Console.WriteLine("длина");
+                case 4:
+                    Console.WriteLine("длина");
+                case 5:
+                    Console.WriteLine("длина");
+                case 6:
+                    Console.WriteLine("длина");
+                case 7:
+                    Console.WriteLine("длина");
+                case 8:
+                    Console.WriteLine("длина");
+                    break;
+
+            }
+        }
+    }
+}
